9bbfeb8ebfb61a27103db3e5917d1d0b95573424
Timothy Lalli <trlalli92@gmail.com>
2020-07-16 15:56:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,8 +5,8 @@ end
 
 
 # n = 2
-  while n < num
-    return false if num < 0 || num % n == 0 
-    n += 1
-  end
-  true
+#   while n < num
+#     return false if num < 0 || num % n == 0 
+#     n += 1
+#   end
+#   true
